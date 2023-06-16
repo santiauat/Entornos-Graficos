693fa47dfbd6feb925b9c1040b9db79136456372
santiauat <santiauat@hotmail.com>
2023-06-16 18:01:57 -0300

    Inicio con ej 2 manejo de BD

---
+++ Php_manejoBD/Ej2/datos.sql
@@ -0,0 +1,22 @@
+INSERT INTO `ciudades` (`id`, `ciudad`, `pais`, `habitantes`, `superficie`, `tieneMetro`) 
+VALUES
+ 
+    (NULL, 'Mexico DF', 'Mexico', '555666', '23434.3', '1'), 
+    
+    (NULL, 'Barcelona', 'España', '444333', '1111.11', '0'), 
+    
+    (NULL, 'Buenos Aires', 'Argentina', '888111', '333.33', '1'), 
+    
+    (NULL, 'Medellin ', 'Colombia', '999222', '888.88', '0'), 
+    
+    (NULL, 'Lima', 'Peru', '999111', '222.22', '1'),
+     
+    (NULL, 'Caracas', 'Venezuela', '111222', '111.11', '1'), 
+    
+    (NULL, 'Santiago', 'Chile', '777666', '222.22', '1'), 
+     
+    (NULL, 'Antigua', 'Guatemala', '444222', '877.33', '0'), 
+    
+    (NULL, 'Quito', 'Ecuador', '333111', '999.11', '1'), 
+     
+    (NULL, 'La Habana', 'Cuba', '111222', '333.11', '0')
